0a2eb584fd9f3e2da04f6d62728d7090cdfcb1d3
YoungLXxx <553282480@qq.com>
2021-09-05 07:24:31 -0700

    将多线程tcp server升级为tls server

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
 	gcc cli_pthread.c -pthread -o client
-	gcc ser_pthread.c -pthread -o server
+	gcc ser_pthread.c -pthread -lssl -lcrypto -o server
 clean:
 	rm -rf *o server client
--- ser_pthread.c
+++ ser_pthread.c
@@ -10,7 +10,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>  
+#include <sys/wait.h>
+#include <arpa/inet.h> 
+#include <openssl/ssl.h>
+#include <openssl/err.h> 
 
 #define DEFAULT_PORT 8000
 #define MAXLINE 4096
@@ -44,15 +47,6 @@ void init(){
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 }
 
-static void wait(void)
-{
-  time_t start_time = time(NULL);
-  printf("%s\n", ctime(&start_time));
-  while (time(NULL) == start_time)
-  {
-    /* do nothing except chew CPU slices for up to one second */
-  }
-}
 
 void *ser_recv(void *t)
 {
@@ -62,16 +56,69 @@ void *ser_recv(void *t)
   sc *fd = NULL;
   fd = (sc*)t;
   int client_socket = fd->c_fd;
+  SSL_CTX *ctx;
+  SSL *ssl;
+
+  /* SSL 库初始化 */
+  SSL_library_init();
+  /* 载入所有 SSL 算法 */
+  OpenSSL_add_all_algorithms();
+  /* 载入所有 SSL 错误消息 */
+  SSL_load_error_strings();
+  /* 用 TLSv1_2_server_method() 单独表示 V2 标准 */
+  ctx = SSL_CTX_new(TLSv1_2_server_method());
+  /* SSLv23_client_method 可以以 SSL V2 和 V3 标准兼容方式产生一个 SSL_CTX ，即 SSL Content Text */
+  if (ctx == NULL) {
+    ERR_print_errors_fp(stdout);
+    exit(1);
+  }
+
+  // 设置信任根证书
+  if (SSL_CTX_load_verify_locations(ctx, "ca.crt",NULL)<=0){
+    ERR_print_errors_fp(stdout);
+    exit(1);
+  }
+  /* 载入用户的数字证书， 此证书用来发送给客户端。 证书里包含有公钥 */
+  if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(stdout);
+    exit(1);
+  }
+  /* 载入用户私钥 */
+  if (SSL_CTX_use_PrivateKey_file(ctx, "server_rsa_private.pem.unsecure", SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(stdout);
+    exit(1);
+  }
+  /* 检查用户私钥是否正确 */
+  if (!SSL_CTX_check_private_key(ctx)) {
+    ERR_print_errors_fp(stdout);
+    exit(1);
+  }
+
+  /* 基于 ctx 产生一个新的 SSL */
+  ssl = SSL_new(ctx);
+  /* 将连接用户的 socket 加入到 SSL */
+  SSL_set_fd(ssl, client_socket);
+  /* 建立 SSL 连接 */
+  if (SSL_accept(ssl) == -1) {
+    perror("accept");
+    close(client_socket);
+    exit(1);
+  }
+
   while (1)
   {
     //接受客户端传过来的数据
-    n = recv(client_socket, buff, MAXLINE, 0);
+    //n = recv(client_socket, buff, MAXLINE, 0);
+    n = SSL_read(ssl,buff,MAXLINE);
     if(n != -1){
       buff[n] = '\0';
       printf("client %d recv msg from client: %s\n", mytid-1, buff);
-      //send
-      if (send(client_socket, buff , n, 0) == -1)
+      //发消息给客户端
+      //if (send(client_socket, buff , n, 0) == -1){
+      if (SSL_write(ssl, buff, strlen(buff)) <= 0){
         perror("send error");
+        break;
+      }
     }
     else{
       printf("client %s:%d:", clientlist[mytid-1].ip, clientlist[mytid-1].port);
@@ -80,9 +127,13 @@ void *ser_recv(void *t)
       task--;
       mytid = 0;
       pthread_exit(NULL);
+      break;
     }
   }
-
+  /* 关闭 SSL 连接 */
+  SSL_shutdown(ssl);
+  /* 释放 SSL */
+  SSL_free(ssl);
 }
 
 int main(int argc, char **argv)
@@ -96,13 +147,16 @@ int main(int argc, char **argv)
   int tid = 0;
   int sin_size = sizeof(struct sockaddr_in);
   struct sockaddr_in remote_addr;
+ 
+  
+
   //初始化Socket
   if ((fd.s_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
     printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
     exit(0);
   }
-  //初始化
+  //建立ocket
   memset(&fd.servaddr, 0, sizeof(fd.servaddr));
   fd.servaddr.sin_family = AF_INET;
   fd.servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。
